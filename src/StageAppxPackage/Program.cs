59944c6e89b8c24ab163e90d8b6dd1ea888f3316
Peter Torr <ptorr@microsoft.com>
2020-05-12 22:35:54 -0700

    Added Stage-AppxPackage to the repo.

---
+++ src/StageAppxPackage/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Windows.Management.Deployment;
+
+namespace StageAppxPackage
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.Error.WriteLine($@"Usage: {Environment.CommandLine[0]} <path-to-package>.
+
+        Do not include the 'AppxManifest.xml' filename.");
+                return 1;
+            }
+
+            if (!Uri.TryCreate(args[0], UriKind.Absolute, out var uri) || uri.Scheme != "file")
+            {
+                Console.Error.WriteLine($"'{args[0]}' is not a valid name for a package; must be convertible to the 'file' URI scheme.");
+                return 2;
+            }
+
+            var result = RealMain(uri).Result;
+            if (result == 0)
+            {
+                Console.WriteLine("Success!");
+            }
+            else
+            {
+                Console.WriteLine("Error.");
+            }
+
+            return result;
+        }
+
+        // Not in the SDK yet
+        const DeploymentOptions StageInPlace = (DeploymentOptions)0x400000;
+
+        static async Task<int> RealMain(Uri packageUri)
+        {
+            try
+            {
+                var pm = new PackageManager();
+                await pm.StagePackageAsync(packageUri, null, StageInPlace);
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex.Message);
+                return 3;
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ src/StageAppxPackage/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Stage-AppxPackage")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Stage-AppxPackage")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("42ee722f-1acc-4c94-a0ef-394de454cf8d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
